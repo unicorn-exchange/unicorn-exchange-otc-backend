24bb16ef70cc2c5610ecec1a6434beffc3bb7bc1
Alex Semenov <skarppi@mail.ru>
2019-09-07 11:23:18 +0300

    update

--- src/api/v1/routes/orders/create.ts
+++ src/api/v1/routes/orders/create.ts
@@ -2,23 +2,27 @@ import {IOrdersCreateReq} from "../../../../types/api/requests";
 import {OrderModel} from "../../../../types/models/order.model";
 import {IOrdersCreateRes} from "../../../../types/api/responses";
 import {ordersCreateValidationScheme} from "../../../../types/validators/orders-create-validator";
+import * as yup from "yup";
 
 export async function ordersCreateCtr(order: IOrdersCreateReq): Promise<IOrdersCreateRes> {
-  const result = ordersCreateValidationScheme.validate(order);
-  if (result.error) {
-    return {
-      ok: false,
-      errors: [result.error],
-    };
-  }
-
-  return OrderModel.create(order)
-    .then(orderInstance => {
-      return {
-        ok: true,
-        data: orderInstance.toJSON(),
-        errors: [],
-      };
+  return yup
+    .object()
+    .shape(ordersCreateValidationScheme)
+    .isValid(order)
+    .then(() => {
+      return OrderModel.create(order).then(orderInstance => {
+        return {
+          ok: true,
+          data: orderInstance.toJSON(),
+          errors: [],
+        };
+      });
+      // .catch(err => {
+      //   return {
+      //     ok: false,
+      //     errors: [err],
+      //   };
+      // });
     })
     .catch(err => {
       return {
